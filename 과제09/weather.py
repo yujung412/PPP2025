488fe68019e2a40d72bd897cbcc584d8a98e2909
yujung412 <yujung412@naver.com>
2025-04-12 22:32:11 +0900

    Add files via upload

---
+++ 과제09/weather.py
@@ -0,0 +1,35 @@
+def read_db(filename):
+    with open(filename, encoding="utf-8-sig") as f:
+        lines = f.readlines()
+
+    temp_list = []
+    rain_list = []
+
+    for line in lines[1:]:
+        parts = line.strip().split(",")
+        rain_list.append(float(parts[1]))
+        temp_list.append(float(parts[4]))
+
+    return temp_list, rain_list
+
+
+def main():
+    temp, rain = read_db("weather(146)_2022-2022.csv")
+
+    total_temp = sum(temp)
+    avg_temp = total_temp / len(temp)
+
+    total_rain = sum(rain)
+    rain_days = 0
+    for r in rain:
+        if r >= 5:
+            rain_days += 1
+
+    print(f"연 평균 기온: {round(avg_temp, 1)}도")
+    print(f"5mm 이상 비 온 날: {rain_days}일")
+    print(f"총 강우량: {round(total_rain, 1)}mm")
+
+
+# 메인 함수 실행
+if __name__ == "__main__":
+    main()
